5dd59ed89cc3f9aa7635082d74de11a30ef3a621
christina <christina.bogdan@gmail.com>
2017-06-12 20:50:19 -0400

    move navbar to js

---
+++ bar.js
@@ -0,0 +1,16 @@
+document.getElementById("navbar").innerHTML = `
+  <div class="image-holder">
+    <IMG SRC="colpic.jpg" WIDTH="170"/>
+</div>
+  <br>
+
+    <a href="index.html">Home</a><br>
+    christina.bogdan@gmail.com<br>
+  <br>
+  <h1 >Links </h1>
+  <a href="resume.pdf">Resume</a><br>
+
+  <h1 >Social Media</h1>
+  <a  href="https://www.linkedin.com/in/christina-bogdan-b5598069/">LinkedIn</a><br>
+  <a  href="https://github.com/christaina">GitHub</a><br>
+  <a href="https://twitter.com/xtinaBog">Twitter</a><br>`;
