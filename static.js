bb3b02feacf2fb99da071ea855154f983fa82fb5
christaina <christina.bogdan@gmail.com>
2017-07-06 13:35:58 -0400

    update colors

---
+++ static.js
@@ -0,0 +1,23 @@
+document.getElementById("navbar").innerHTML = `
+  <div class="image-holder">
+    <IMG SRC="colpic.jpg" WIDTH="170"/>
+</div>
+  <br>
+
+
+    christina.bogdan@gmail.com<br>
+  <br>
+  <h1 >Links </h1>
+  <a href="main.html">Home</a><br>
+  <a href="resume.pdf" target='_blank'>Resume</a><br>
+  <a href="projects.html">Projects</a><br>
+
+  <h1 >Social Media</h1>
+  <a  href="https://www.linkedin.com/in/christina-bogdan-b5598069/" target='_blank'>LinkedIn</a><br>
+  <a  href="https://github.com/christaina" target='_blank'>GitHub</a><br>
+  <a href="https://twitter.com/xtinaBog" target='_blank'>Twitter</a><br>`;
+
+document.getElementById("heading").innerHTML = `
+<h1>Christina Bogdan</h1>
+<h2>Data Scientist</h2>
+<hr>`
